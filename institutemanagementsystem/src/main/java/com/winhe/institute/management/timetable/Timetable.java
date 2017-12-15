73889fb37e5d075021165c87ed02395f9e30783e
Themiya77 <themiyaranasinghe1@gmail.com>
2017-12-15 14:46:36 +0530

    done a few changes

--- institutemanagementsystem/src/main/java/com/winhe/institute/management/timetable/Timetable.java
+++ institutemanagementsystem/src/main/java/com/winhe/institute/management/timetable/Timetable.java
@@ -1,5 +1,15 @@
 package com.winhe.institute.management.timetable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
 public class Timetable {
 
+	@Id
+	@SequenceGenerator(name="timetable_generator", sequenceName="timetable_seq", allocationSize = 1, initialValue = 1000)
+	@GeneratedValue(strategy= GenerationType.SEQUENCE, generator="timetable_generator")
+	private Long id;
+	
 }
