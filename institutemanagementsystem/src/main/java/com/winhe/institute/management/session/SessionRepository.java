826e880aa753d1b72dd0f461994a0f9b300ccd4c
bhanukaManesha <bhanukamgamage@gmail.com>
2017-12-20 11:38:17 +0530

    Added the Valdiation and Student Find All Method

--- institutemanagementsystem/src/main/java/com/winhe/institute/management/session/SessionRepository.java
+++ institutemanagementsystem/src/main/java/com/winhe/institute/management/session/SessionRepository.java
@@ -1,9 +1,10 @@
 package com.winhe.institute.management.session;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
-public interface SessionRepository extends CrudRepository<Session,Long>{
+@Repository
+public interface SessionRepository extends JpaRepository<Session,Long>{
 
 	public Session findByToken(String token);
 }
